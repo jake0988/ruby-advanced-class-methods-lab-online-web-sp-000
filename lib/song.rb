4d624497e1531cc5210fa338b65d23c3005b97ea
Jake G <jake098877@gmail.com>
2020-05-07 11:31:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,21 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    @@all << self.new
+  end
+
+  def self.new_by_name(name)
+    name = name.name
+    self.name
+  end
+
+  def self.create_by_name(name)
+    @name = name
+    @@all << @name
+    self.name
+  end
+
   def self.all
     @@all
   end
