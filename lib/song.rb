39c2e831fc535beadef51c3eca8ac83a17ea4a93
Jake G <jake098877@gmail.com>
2020-05-07 15:33:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,8 @@ class Song
      @@all.sort_by { |song| song.name }
   end
 
-  def self.new_from_filename
-    @song = self.new
-    @artist = self.new
+  def self.new_from_filename 
+    file = self.new
+    file = "#{artist} - {name}"
+
 end
