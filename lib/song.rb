dd3ab27e2395e8f42bd882e65b6cbeaff4622c76
Jake G <jake098877@gmail.com>
2020-05-07 15:52:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   def self.new_from_filename(string)
     file = self.new
      string = string.split(" - ")
-     string = string.slice!(".mp3")
+     string = string.slice!(-4)
      artist_name = string[0]
      song = string[1]
      file.name = song
