fbbee6390224213987a1362fe5182221013742e2
Jake G <jake098877@gmail.com>
2020-05-07 11:45:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
     @song
   end
 
-  def self.new_by_name(name)
-    @song = self.name
+  def self.new_by_name(name)    
+    @song = @song.name
   end
 
   def self.create_by_name(name)
